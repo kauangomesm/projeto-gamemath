6ee4467a080df772dc3990c762a07ff547177d44
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-06-23 23:57:35 -0300

    Update gameMath.js

--- gameMath.js
+++ gameMath.js
@@ -1,6 +1,7 @@
-for(let c = 0; c < 3; c++){
-    // let op = parseInt(Math.random() * 7)
-    let op = 3
+let acertos = 0
+let question = 1
+for(let c = 0; c < 10; c++){
+    let op = parseInt(Math.random() * 7)
     let pergunta = ''
     let n1 = 0
     let n2 = 0
@@ -10,20 +11,20 @@ for(let c = 0; c < 3; c++){
              n1 = parseInt(Math.random() * 10)
              n2 = parseInt(Math.random() * 10)
              soma = (n1+n2)
-            pergunta = (`quanto e ${n1} + ${n2}`)
+            pergunta = (`questão ${question}: quanto e ${n1} + ${n2}`)
             break;
         
         case 1:
              n1 = parseInt(Math.random() * 10)
              n2 = parseInt(Math.random() * 10)
              soma = (n1-n2)
-            pergunta = (`quanto e ${n1} - ${n2}`)
+            pergunta = (`questão ${question}: quanto e ${n1} - ${n2}`)
             break;
         case 2:
              n1 = parseInt(Math.random() * 10)
              n2 = parseInt(Math.random() * 10)
              soma = (n1*n2)
-            pergunta = (`quanto e ${n1} x ${n2}`)
+            pergunta = (`questão ${question}: quanto e ${n1} x ${n2}`)
             break;
         case 3:
              while(n1<n2 || n1%n2 != 0 || n1===n2 || n2===1){
@@ -31,18 +32,67 @@ for(let c = 0; c < 3; c++){
                 n2 = parseInt(Math.random() * 100)
              }
              soma = n1/n2
-            //  alert(soma)
-             pergunta = (`quanto e ${n1} / ${n2}`)       
+             pergunta = (`questão ${question}: quanto e ${n1} / ${n2}`) 
+             break
+        case 4:
+            while(n1 === 0 || n1 === 1 || n2 === 0 || n2 === 1){
+            n1 = parseInt(Math.random() * 10)
+            n2 = parseInt(Math.random() * 3)
+            }
+            soma = n1**n2
+             pergunta = (`questão ${question}: quanto e ${n1}^${n2}`)
+             break
+        case 5:
+            while(parseInt(soma) != soma || n1 === 0 || n1 === 1){
+                n1 = parseInt(Math.random() * 100)
+                n2 = parseInt(Math.random() * 2)
+                if(n2 === 0){
+                    soma = Math.pow(n1, 0.5)
+                    pergunta = (`questão ${question}: quanto e  2√${n1}`)
+                }
+                else{
+                    soma = Math.pow(n1, 1/3)
+                    pergunta = (`questão ${question}: quanto e  3√${n1}`)
+                }
+            }
+            break
+        case 6:
+            n1 = parseInt(Math.random() * 7)
+            n2 = n1
+            if (n1 === 0 || n1 === 1){
+                soma = 1
+                pergunta = (`questão ${question}: quanto e ${n1}!`)
+            }
+            else{
+                for(let g = 1; g < n1; g++){
+                    n2 *= g
+                }
+                soma = n2
+                console.log(soma)
+                pergunta = (`questão ${question}: quanto e ${n1}!`)
+            }
+            break
     }
     let resposta = prompt(pergunta)
     if (resposta != soma){
         alert('Que pena vc errou')
         break
     }
-
-
+    else{
+        acertos++
+    }
+    question++
 }
-
-
-
-//Crie um jogo matematico em javascript que que gera contas aleatórios para que o usuario responda, com operadores aleatorios (+ - x ÷ * √ !) e que quando o usuario responder 10 contas em seguida apareça uma mensagem ma tela dizendo que ele e um "jenio" igual o Jordan, e se ele errar alguma conta apareça que ele e um jenio ingual o careca do oliver
+alert('Acabou')
+if(acertos === 10){
+    alert(`vc acertou ${acertos} questoes: Parece que temos um 'Jenio' aqui parece ate que e o Jordan`)
+}
+else if(acertos >=5){
+    alert(`vc acertou ${acertos} questoes: Não e um jenio mas da para o gasto`)
+}
+else if(acertos != 0){
+    alert(`vc acertou ${acertos} questoes: precisa estudar mais `)
+}
+else{
+    alert(`vc acertou ${acertos} questoes: So digo uma coisa E BOMBA!!!`)
+}
