cdbf2d607d38ce38ffd87066548bb62adc363b31
kauan gomes marques <109297933+kauangomesm@users.noreply.github.com>
2023-06-30 17:55:58 -0300

    .

---
+++ projeto-gamemath-teste/jogo.js
@@ -0,0 +1,9 @@
+const questao = document.querySelector("#cabecalho p")
+const perguntar = document.querySelector("#form p")
+const resposta = document.querySelector("#form #numero")
+const telaInicial = document.querySelector("#telainicial")
+const tela = document.querySelector("#tela")
+function comecar(){
+    telaInicial.style.display = "none"
+    tela.style.display = "flex"
+}
